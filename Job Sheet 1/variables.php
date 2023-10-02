b1b9bb95e033ddc835b52288b1eba46f63457c7c
Puputera <127200098+Puputera@users.noreply.github.com>
2023-10-02 13:40:21 +0700

    Create variables.php

---
+++ Job Sheet 1/variables.php
@@ -0,0 +1,20 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variables</title>
+</head>
+<body>
+    <?php
+        //variabel dengan tipe data
+        $integer = 25;
+        $string = "Praktikum Web II";
+        $boolean = true;
+
+        //menampilkan dilayar
+        echo "Ini contoh Integer $integer </br>";
+        echo "Ini conroh String $string </br>";
+        echo "Ini contoh Boolean $boolean </br>";
+    ?>
+</body>
+</html>
